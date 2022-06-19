f7fd2ae8d37ba240efc859eddf4e5de0e0258e47
Lunga Tsewu <81965280+tswlun002@users.noreply.github.com>
2022-06-19 16:58:34 +0200

    ..

--- WorkPackage1/Prac-01/src/BinClock.h
+++ WorkPackage1/Prac-01/src/BinClock.h
@@ -17,16 +17,16 @@ void minInc(void);
 void toggleTime(void);
 
 // define constants
-const char RTCAddr = //Input RTC Address here 
+const char RTCAddr =0x68 //Input RTC Address here 
 const char SEC_REGISTER = 0x00; // see register table in datasheet
 const char MIN_REGISTER = 0x01;
 const char HOUR_REGISTER = 0x02;
 const char TIMEZONE = 2; // +02H00 (RSA)
 
 // define pins
-const int LED = //CHoose your LED Pin here
+const int LED = 4;//CHoose your LED Pin here
 const int SECS = 1;
-const int BTNS[] = {5,30}; // B0, B1
+const int BTNS[] = {0,2}; // B0, B1
 
 
 #endif
